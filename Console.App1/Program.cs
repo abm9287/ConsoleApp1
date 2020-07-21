32d263cb73c0933f8722471112a8907f78f415e7
venom <venom@DESKTOP-VI6EVSE>
2020-07-21 14:34:56 -0500

    Herencia

---
+++ Console.App1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using App1.Logica;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!");
+
+            var empresa = new Empresa();
+
+            var estudiante = new Persona("Steven", "Chavez")
+            {
+                Celular = empresa.ConstruirCelular("Samsung")
+            };
+
+            estudiante.Automovil = empresa.ConstruirAutomovil("Sandero");
+            estudiante.Bicicleta = empresa.ConstruirBicicleta("GT", "Karakoram", 900);
+
+            Console.WriteLine(estudiante.Saludar());
+            Console.WriteLine(estudiante.IndicarCelular());
+            Console.WriteLine(estudiante.Bicicleta.Frenar());
+
+            Console.WriteLine("Ingresa tu nombre: ");
+            var nombre = Console.ReadLine();
+
+            var saludo = estudiante.SaludarPersona(nombre);
+            Console.WriteLine(saludo);
+            Console.ReadKey();
+        }
+    }
+}
